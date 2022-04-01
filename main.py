21076e64c5d6874f724fff41fdd5c7810a43721b
msheikh05 <102785329+msheikh05@users.noreply.github.com>
2022-04-01 06:05:34 +0000

    Added while loop to repeat sending of email

--- main.py
+++ main.py
@@ -1,26 +1,18 @@
 import yagmail
 import os
 import time
-import schedule
 
-def send_email():
+sender = 'sheikh212@gmail.com'
+receiver = '35bw29dv@minimail.gq'
+  
+subject = "This is the subject"
+  
+contents = """Here is the content of the email
+Hi!
+"""
 
-  sender = 'sheikh212@gmail.com'
-  receiver = '35bw29dv@minimail.gq'
-  
-  subject = "This is the subject"
-  
-  contents = """
-  Here is the content of the email
-  Hi!
-  """
-  
+while True:
   yag = yagmail.SMTP(user=sender,password= os.getenv('PASSWORD'))
   yag.send(to=receiver,subject=subject,contents=contents)
   print("Email sent")
-
-schedule.every(9).seconds.do(send_email)
-
-while True:
-  schedule.run_pending()
-  time.sleep(1)
+  time.sleep(10)
